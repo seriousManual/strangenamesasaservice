e2c53458624799ef9f301244d5292d7d73a8a1bb
zaphod1984 <zaphod84@gmx.de>
2013-08-01 22:17:35 +0200

    waiting

--- page/js/script.js
+++ page/js/script.js
@@ -1,11 +1,17 @@
+var $waiting = $('<img>').attr('src', '/img/waiting.gif');
+
 function callAPI(category, url, container) {
     container.removeClass('errorClass');
+    container.html('<img src="/img/waiting.gif">');
 
     ga('send', 'event', 'tryit', category, url);
 
     $.get(url)
         .done(function(data) {
-            container.text(JSON.stringify(data));
+                setTimeout(function() {
+                    container.text(JSON.stringify(data));
+                }, 2000);
+
         })
         .fail(function(data, state, message) {
             container.addClass('errorClass');
