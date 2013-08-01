04cd053792e3ede4accfe92458e5b824d1c308c9
zaphod1984 <zaphod84@gmx.de>
2013-08-01 13:51:28 +0200

    logger

--- api/logger.js
+++ api/logger.js
@@ -2,9 +2,4 @@ var util = require('util');
 
 var winston = require('winston');
 
-winston.remove(winston.transports.Console);
-winston.add(winston.transports.Console, {
-    timestamp: true
-});
-
 module.exports = winston;
